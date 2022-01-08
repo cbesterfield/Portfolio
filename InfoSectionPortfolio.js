c1bc3156a39beaf46f4b90e970925939e65586c7
Cole Besterfield <67971371+cbesterfield@users.noreply.github.com>
2022-01-08 16:29:25 -0500

    info level

---
+++ InfoSectionPortfolio.js
@@ -0,0 +1,95 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Button } from '../Button'
+import {FaGithub} from 'react-icons/fa'
+import {css} from 'styled-components/macro'
+
+
+const Section = styled.section`
+width: 100%;
+height: 100%;
+padding: 4rem 0rem;
+`;
+const Container = styled.div`
+padding: 3 rem calc((100vw - 1300px) / 2);
+display: grid;
+grid-template-columns: 1fr 1fr;
+grid-template-rows: 800px;
+
+@media screen and (max-width: 768px) {
+    grid-template-columns: 1fr;
+}
+`;
+const ColumnLeft = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-content: flex-start;
+line-height: 1.4;
+padding: 1rem 2rem;
+order: ${({reverse}) => (reverse ? '2' : '1')};
+
+h1 {
+    margin-bottom: 1rem;
+    font-size: clamp(1.5rem, 6vw, 2rem);
+}
+
+p {
+    margin-bottom: 2rem;  
+}
+`;
+const ColumnRight = styled.div`
+padding: 1rem 2rem;
+order: ${({reverse}) => (reverse ? '1' : '2')};
+display: flex;
+justify-content: center;
+align-items: center;
+
+@media screen and (max-width: 768px) {
+    order: ${({reverse}) => (reverse ? '2' : '1')};
+}
+
+img {
+    max-width: 500px;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+
+    @media screen and (max-width: 768px) {
+        width: 90%;
+        heigh: 90%;
+
+    }   
+}
+`;
+
+const Icons = css`
+font-size: clamp(1rem, 6vw, 2rem);
+margin-right: 1.5rem;
+color: #7f00ff;
+`;
+
+const Github = styled(FaGithub)`
+${Icons}
+`;
+
+const InfoSection = ({heading, paragraphOne, paragraphTwo, buttonLabel, reverse, image}) => {
+    return (
+        <Section>
+            <Container>
+                <ColumnLeft>
+                    <h1>{heading}</h1>
+                    <p>{paragraphOne}</p>
+                    <a href='https://www.github.com/cbesterfield/Portfolio' rel='noreferrer' target='_blank'>
+                            <Github />
+                        </a>
+                </ColumnLeft>
+                <ColumnRight reverse={reverse}>
+                    <img src={image} alt='Project' />
+                </ColumnRight>
+            </Container>
+        </Section>
+    )
+}
+
+export default InfoSection
